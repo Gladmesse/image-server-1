93e0ceaf51ae6e599b566fe4341220303ed0e871
Gladmesse <michael.c@gladfagskole.dk>
2025-08-20 22:09:51 +0200

    browser ændringer

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require("cors");
 const multer = require('multer');
 const path = require('path');
 const app = express();
+app.use(cors());
 
 app.use('/uploads', express.static('uploads'));
 
